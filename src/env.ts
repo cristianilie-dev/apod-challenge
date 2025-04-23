c9cc9d5c365d9e7343d6a47b3a5204424539ee10
Cristian <cristianilie.dev@gmail.com>
2025-04-23 11:41:02 +0300

    Env variables init

--- src/env.ts
+++ src/env.ts
@@ -14,6 +14,8 @@ export const env = createEnv({
 
   client: {
     VITE_APP_TITLE: z.string().min(1).optional(),
+    VITE_APOD_BASE_URL: z.string().url(),
+    VITE_APOD_API_KEY: z.string().min(1),
   },
 
   /**
