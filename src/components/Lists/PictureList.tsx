651a56d26a24abd14517540bdc8e4e11dfce7ad0
Cristian <cristianilie.dev@gmail.com>
2025-04-23 21:02:39 +0300

    PictureList strings update

--- src/components/Lists/PictureList.tsx
+++ src/components/Lists/PictureList.tsx
@@ -1,18 +1,23 @@
 
 type ListProps = {
-  children: React.ReactNode;
+  title?: string;
+  description?: string;
+  subtitle?: string;
+  children?: React.ReactNode;
 };
 
-export default function PictureList({ children }: ListProps) {
+export default function PictureList({ title, description, subtitle, children }: ListProps) {
   return (
     <div className="mx-2 sm:mx-auto sm:max-w-3xl">
-      <h1 className="text-2xl font-bold mb-2">Title</h1>
-      <p className="mb-4 text-sm opacity-70">Description</p>
+      {title && <h1 className="text-2xl font-bold mb-2">{title}</h1>}
+      {description && <p className="mb-4 text-sm opacity-70">Description</p>}
 
       <ul className="bg-base-100 rounded-box shadow-md border-0 sm:border sm:border-base-300">
-        <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
-          Subtitle
-        </li>
+        {subtitle && (
+          <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
+            Subtitle
+          </li>
+        )}
 
         {children}
       </ul>
