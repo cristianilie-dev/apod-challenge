bc41c0f51b5e422107f22cb441ddcf748a367a3a
Cristian <cristianilie.dev@gmail.com>
2025-04-23 14:47:19 +0300

    Apod Slice selectedApod

--- src/state/apod/apodSlice.ts
+++ src/state/apod/apodSlice.ts
@@ -1,16 +1,28 @@
 import type { ApodItem } from '@/types/Apod'
 import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
 
 export interface ApodState {
+  selectedApod?: ApodItem
 }
 
 const initialState: ApodState = {
+  selectedApod: undefined
 }
 
 export const apodSlice = createSlice({
   name: 'apod',
   initialState,
-  reducers: {},
+  reducers: {
+    setSelectedApod: (state, action: PayloadAction<ApodItem>) => {
+      state.selectedApod = action.payload
+    },
+    clearSelectedApod: (state) => {
+      state.selectedApod = undefined
+    },
+  },
 })
+
+export const { setSelectedApod, clearSelectedApod } = apodSlice.actions
 
 export default apodSlice.reducer
