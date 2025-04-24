c140047b5b9c0f950f6ae42678fceee1f7264972
Cristian <cristianilie.dev@gmail.com>
2025-04-24 15:18:29 +0300

    ApodList placeholder and buttons updates

--- src/features/apod/ApodList.tsx
+++ src/features/apod/ApodList.tsx
@@ -1,41 +1,72 @@
-import PictureItem, { type PictureItemData } from "@/components/ListItems/PictureItem";
-import PictureList from "@/components/Lists/PictureList";
+import ClearDateButton from "@/components/buttons/ClearDateButton";
+import DatePicker from "@/components/date-pickers/DatePicker";
+import PictureItem from "@/components/list-items/picture-item/PictureItem";
+import PictureList from "@/components/lists/picture-list/PictureList";
+import PictureListPlaceholder from "@/components/lists/picture-list/PictureListPlaceholder";
 import { useGetApodQuery } from "@/state/apod/apodApiSlice"
-import { setSelectedApod } from "@/state/apod/apodSlice";
-import type { AppDispatch } from "@/state/store";
+import { clearSelectedDate, setSelectedApod, setSelectedDate } from "@/state/apod/apodSlice";
+import type { AppDispatch, RootState } from "@/state/store";
 import type { ApodItem } from "@/types/Apod";
 import { formatApiDate, getOneWeekAgoDate } from "@/utils/date";
 import { useNavigate } from "@tanstack/react-router";
 import { useMemo } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function ApodList() {
-  const formattedDate = useMemo(() => formatApiDate(getOneWeekAgoDate()), [])
-  const { data: apodData, isLoading } = useGetApodQuery({ start_date: formattedDate })
+  // State
+  const selectedDate = useSelector((state: RootState) => state.apod.selectedDate);
+  const formattedApodParams = useMemo(() =>
+    selectedDate
+      ? { date: selectedDate }
+      : { start_date: formatApiDate(getOneWeekAgoDate()) }, [selectedDate])
+  const { data: apodData, isFetching } = useGetApodQuery(formattedApodParams);
+
   const dispatch = useDispatch<AppDispatch>()
   const navigate = useNavigate()
 
-
+  // Events
   const handleClick = (item: ApodItem) => {
     dispatch(setSelectedApod(item))
     navigate({ to: `/apod/$date`, params: { date: item.date } })
   }
 
-  if (isLoading) return <div>Loading...</div>
+  const handleDateChange = (date?: Date) => {
+    if (!date) return;
+    const formattedDate = formatApiDate(date)
+    dispatch(setSelectedDate(formattedDate))
+  }
+  const handleClearDate = () => dispatch(clearSelectedDate())
+
+  // Render
   if (!apodData?.length) return <div>No data available</div>
 
   return <>
-    <PictureList>
-      {/* TODO */}
-      {apodData.slice().reverse().map((item) => (
-        <PictureItem
-          key={item.date}
-          title={item.title}
-          imageSrc={item.url}
-          date={item.date}
-          onClick={() => handleClick(item)}
-        />
-      ))}
-    </PictureList>
+    <div className="flex justify-center items-center gap-2 py-4">
+      <DatePicker
+        date={selectedDate ? new Date(selectedDate) : undefined}
+        onDateChange={handleDateChange}
+      />
+
+      {selectedDate && (
+        <ClearDateButton onClick={handleClearDate} />
+      )}
+    </div>
+
+    {isFetching ? (
+      <PictureListPlaceholder />
+    ) : (
+      <PictureList>
+        {/* TODO */}
+        {apodData.slice().reverse().map((item) => (
+          <PictureItem
+            key={item.date}
+            title={item.title}
+            imageSrc={item.url}
+            date={item.date}
+            onClick={() => handleClick(item)}
+          />
+        ))}
+      </PictureList>
+    )}
   </>
 }
