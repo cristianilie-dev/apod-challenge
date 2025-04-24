9242011aa0bca903ac64254f84caf6eae3b6a5ba
Cristian <cristianilie.dev@gmail.com>
2025-04-24 15:17:15 +0300

    PictureItem & List update

--- src/components/lists/picture-list/PictureList.tsx
+++ src/components/lists/picture-list/PictureList.tsx
@@ -10,12 +10,12 @@ export default function PictureList({ title, description, subtitle, children }: 
   return (
     <div className="mx-2 sm:mx-auto sm:max-w-3xl">
       {title && <h1 className="text-2xl font-bold mb-2">{title}</h1>}
-      {description && <p className="mb-4 text-sm opacity-70">Description</p>}
+      {description && <p className="mb-4 text-sm opacity-70">{description}</p>}
 
       <ul className="bg-base-100 rounded-box shadow-md border-0 sm:border sm:border-base-300">
         {subtitle && (
           <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
-            Subtitle
+            {subtitle}
           </li>
         )}
 
