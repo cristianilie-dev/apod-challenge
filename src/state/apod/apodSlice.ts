28e19eeb3f3fe67498f25c7873ff8f266f341725
Cristian <cristianilie.dev@gmail.com>
2025-04-25 12:11:53 +0300

    ApodSlice week offset implementation

--- src/state/apod/apodSlice.ts
+++ src/state/apod/apodSlice.ts
@@ -1,37 +1,57 @@
-import type { ApodItem } from '@/types/Apod'
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+import type { ApodItem } from '@/types/Apod';
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 export interface ApodState {
-  selectedApod?: ApodItem
-  selectedDate?: string
+  selectedApod?: ApodItem;
+  selectedDate?: string;
+  weekOffset: number;
 }
 
 const initialState: ApodState = {
   selectedApod: undefined,
-  selectedDate: undefined
-}
+  selectedDate: undefined,
+  weekOffset: 0,
+};
 
 export const apodSlice = createSlice({
   name: 'apod',
   initialState,
   reducers: {
     setSelectedApod: (state, action: PayloadAction<ApodItem>) => {
-      state.selectedApod = action.payload
+      state.selectedApod = action.payload;
     },
     clearSelectedApod: (state) => {
-      state.selectedApod = undefined
+      state.selectedApod = undefined;
     },
 
     setSelectedDate: (state, action: PayloadAction<string>) => {
-      state.selectedDate = action.payload
+      state.selectedDate = action.payload;
     },
     clearSelectedDate: (state) => {
-      state.selectedDate = undefined
+      state.selectedDate = undefined;
+    },
+
+    setWeekOffset: (state, action: PayloadAction<number>) => {
+      state.weekOffset = action.payload;
+    },
+    incrementWeekOffset: (state) => {
+      state.weekOffset += 1;
+    },
+    decrementWeekOffset: (state) => {
+      state.weekOffset = Math.max(0, state.weekOffset - 1);
     },
   },
-})
+});
 
-export const { setSelectedApod, clearSelectedApod, setSelectedDate, clearSelectedDate } = apodSlice.actions
+export const {
+  setSelectedApod,
+  clearSelectedApod,
+  setSelectedDate,
+  clearSelectedDate,
+  setWeekOffset,
+  incrementWeekOffset,
+  decrementWeekOffset,
+} = apodSlice.actions;
 
-export default apodSlice.reducer
+export default apodSlice.reducer;
