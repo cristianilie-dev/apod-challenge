b3697406345c88d0454518bd86ac89928d13974b
Cristian <cristianilie.dev@gmail.com>
2025-04-25 12:12:07 +0300

    ApodApiSlice end_date support

--- src/state/apod/apodApiSlice.ts
+++ src/state/apod/apodApiSlice.ts
@@ -1,11 +1,12 @@
-import { env } from '@/env'
-import type { ApodItem } from '@/types/Apod'
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { ApodItem } from '@/types/Apod';
+import { env } from '@/env';
 
 type GetApodParams = {
-  start_date?: string
-  date?: string
-}
+  date?: string;
+  start_date?: string;
+  end_date?: string;
+};
 
 export const apodApiSlice = createApi({
   reducerPath: 'apodApi',
@@ -15,20 +16,21 @@ export const apodApiSlice = createApi({
   }),
 
   endpoints: (builder) => ({
-    getApod: builder.query<ApodItem[], GetApodParams>({
-      query: ({ date, start_date }) => {
+    getApod: builder.query<Array<ApodItem>, GetApodParams>({
+      query: ({ date, start_date, end_date }) => {
         const params = new URLSearchParams({
           api_key: env.VITE_APOD_API_KEY,
         });
 
         if (date) params.append('date', date);
         if (start_date) params.append('start_date', start_date);
+        if (end_date) params.append('end_date', end_date);
 
         return {
           url: `/planetary/apod?thumbs=true&${params.toString()}`,
         };
       },
-      transformResponse: (response: ApodItem | ApodItem[]) => {
+      transformResponse: (response: ApodItem | Array<ApodItem>) => {
         return Array.isArray(response) ? response : [response];
       },
     }),
