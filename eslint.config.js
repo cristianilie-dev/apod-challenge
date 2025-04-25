59e21bdfac7733cf5d67c434f5589a77dd333959
Cristian <cristianilie.dev@gmail.com>
2025-04-25 11:53:03 +0300

    Prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,23 @@
 //  @ts-check
 
-import { tanstackConfig } from '@tanstack/eslint-config'
+import { tanstackConfig } from '@tanstack/eslint-config';
+import prettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
-export default [...tanstackConfig]
+export default [
+  {
+    ignores: ['tailwind.config.js'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': ['error', { semi: true, singleQuote: true }],
+      semi: ['error', 'always'],
+    },
+  },
+  prettier,
+  ...tanstackConfig,
+];
