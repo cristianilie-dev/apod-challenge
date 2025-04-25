e75af06384983ce97220973432ec35a3ad7e0aad
Cristian <cristianilie.dev@gmail.com>
2025-04-25 12:14:26 +0300

    BackButton text update

--- src/components/buttons/BackButton.tsx
+++ src/components/buttons/BackButton.tsx
@@ -1,12 +1,11 @@
-
 type BackButtonProps = {
   onClick: () => void;
-}
+};
 
 export function BackButton({ onClick }: BackButtonProps) {
   return (
     <button
-      className="btn btn-ghost text-sm flex items-center gap-2"
+      className="btn btn-ghost text-sm sm:text-base flex items-center gap-2"
       onClick={onClick}
     >
       <span className="text-lg">←</span>
