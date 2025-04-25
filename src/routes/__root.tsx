1f8bda35ef6b3ede7be8373ec52296caa3d39f3d
Cristian <cristianilie.dev@gmail.com>
2025-04-25 12:12:52 +0300

    Background image and title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,11 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
-import Header from '../components/Header'
+import { Outlet, createRootRoute } from '@tanstack/react-router';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <Header />
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 -z-20 bg-[url('/images/background.png')] bg-cover bg-fixed bg-center" />
+
       <Outlet />
-    </>
+    </div>
   ),
-})
+});
--- src/routes/apod/index.tsx
+++ src/routes/apod/index.tsx
@@ -1,10 +1,26 @@
-import { createFileRoute } from '@tanstack/react-router'
-import ApodList from '../../features/apod/ApodList'
+import { createFileRoute } from '@tanstack/react-router';
+import { z } from 'zod';
+import ApodList from '@/features/apod/apod-list/ApodList';
 
 export const Route = createFileRoute('/apod/')({
   component: ApodComponent,
-})
+  validateSearch: z.object({
+    date: z.string().optional(),
+    week_offset: z.number().optional(),
+  }),
+});
 
 function ApodComponent() {
-  return <ApodList />
+  return (
+    <div className="mx-2 sm:mx-auto sm:max-w-3xl pt-8">
+      <h1 className="text-xl sm:text-2xl font-bold mb-2 text-center">
+        Astronomy Picture of the Day
+      </h1>
+      <p className="mb-4 text-xs sm:text-sm opacity-70 text-center">
+        Discover the wonders of the universe through stunning space imagery, updated daily.
+      </p>
+
+      <ApodList />
+    </div>
+  );
 }
