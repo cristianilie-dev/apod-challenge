7a7a4582d298dec33301e269937eca9681c68391
Cristian <cristianilie.dev@gmail.com>
2025-04-25 12:15:14 +0300

    ApodDetailView responsive text updatet

--- src/features/apod/apod-details/ApodDetailView.tsx
+++ src/features/apod/apod-details/ApodDetailView.tsx
@@ -1,35 +1,48 @@
-import LazyImage from "@/components/ui/LazyImage";
+import LazyImage from '@/components/ui/LazyImage';
 
 type ApodDetailViewProps = {
-  hdurl: string;
+  url?: string;
   title: string;
   date: string;
   explanation: string;
 };
 
-export function ApodDetailView({ hdurl, title, date, explanation }: ApodDetailViewProps) {
+export function ApodDetailView({
+  url,
+  title,
+  date,
+  explanation,
+}: ApodDetailViewProps) {
   return (
-    <>
-      <div className="max-w-3xl mx-auto p-4">
-        <div className="card bg-base-100 shadow-xl mb-6">
+    <div className="max-w-3xl mx-auto p-4">
+      <div className="relative mb-6">
+        <div className="absolute inset-0 -z-10 rounded-xl blur-2xl bg-white/10"></div>
 
-          <LazyImage
-            src={hdurl}
-            alt={title}
-            imageClassName="object-cover w-full rounded-t-lg"
-          />
+        <div className="card bg-base-100 shadow-xl">
+          {url && (
+            <LazyImage
+              src={url}
+              alt={title}
+              imageClassName="object-cover w-full rounded-t-lg"
+            />
+          )}
 
-          <div className="card-body justify-end text-white bg-opacity-60 bg-black rounded-b-lg">
-            <h2 className="card-title text-3xl">{title}</h2>
-            <p className="opacity-80">{date}</p>
+          <div
+            className={`card-body justify-end text-white bg-opacity-60 ${url ? 'rounded-b-lg' : 'rounded'
+              }`}
+          >
+            <h2 className="card-title text-xl sm:text-3xl">{title}</h2>
+            <p className="text-xs sm:text-sm opacity-80">{date}</p>
           </div>
         </div>
-
-        <div className="prose prose-sm sm:prose lg:prose-lg max-w-none">
-          <h3 className="text-xs uppercase font-semibold opacity-60">Details</h3>
-          <p>{explanation}</p>
-        </div>
       </div>
-    </>
+
+      <div className="prose prose-xs sm:prose-sm lg:prose max-w-none text-white">
+        <h3 className="text-[0.6rem] sm:text-xs uppercase font-semibold opacity-60">
+          Details
+        </h3>
+        <p>{explanation}</p>
+      </div>
+    </div>
   );
 }
