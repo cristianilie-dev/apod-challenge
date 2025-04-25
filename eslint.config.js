15c6b9356b90683fed4d7e6de40a9a0691edc956
Cristian <cristianilie.dev@gmail.com>
2025-04-25 12:26:07 +0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,39 @@
-//  @ts-check
+// @ts-check
 
-import { tanstackConfig } from '@tanstack/eslint-config';
-import prettier from 'eslint-config-prettier';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
+import { tanstackConfig } from '@tanstack/eslint-config'
+import prettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 export default [
   {
-    ignores: ['tailwind.config.js'],
+    ignores: [
+      'tailwind.config.js',
+      'vite.config.js',
+      'eslint.config.js',
+      'prettier.config.js',
+    ],
   },
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': ['error', { semi: true, singleQuote: true }],
+      semi: ['error', 'always'],
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parser: 'espree', // Default JS parser
+    },
     plugins: {
       prettier: eslintPluginPrettier,
     },
@@ -20,4 +44,4 @@ export default [
   },
   prettier,
   ...tanstackConfig,
-];
+]
