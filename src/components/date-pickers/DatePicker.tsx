1b3fee36386e16b847e47f8d77b945c415ba7655
Cristian <cristianilie.dev@gmail.com>
2025-04-25 12:07:55 +0300

    DatetPicker update

--- src/components/date-pickers/DatePicker.tsx
+++ src/components/date-pickers/DatePicker.tsx
@@ -1,5 +1,5 @@
-import { useMemo, useState } from "react";
-import { DayPicker } from "react-day-picker";
+import { useMemo, useState } from 'react';
+import { DayPicker } from 'react-day-picker';
 
 type DatePickerProps = {
   date?: Date;
@@ -14,27 +14,33 @@ export default function DatePicker({ date, onDateChange }: DatePickerProps) {
     <>
       <button
         popoverTarget="rdp-popover"
-        className="input input-border"
-        style={{ anchorName: "--rdp" } as React.CSSProperties}
+        className="input input-border text-sm sm:text-base"
+        style={{ anchorName: '--rdp' } as React.CSSProperties}
         onClick={() => setIsOpen(true)}
       >
-        {date ? date.toLocaleDateString() : "Pick a date"}
+        {date ? date.toLocaleDateString() : 'Pick a date'}
       </button>
 
-
-      {isOpen && <div popover="auto" id="rdp-popover" className="dropdown" style={{ positionAnchor: "--rdp" } as React.CSSProperties}>
-        <DayPicker
-          className="react-day-picker"
-          mode="single"
-          selected={date}
-          hidden={{ after: today }}
-          onSelect={(date) => {
-            setIsOpen(false);
-            onDateChange?.(date)
-          }}
-          endMonth={today}
-        />
-      </div>}
+      {isOpen && (
+        <div
+          popover="auto"
+          id="rdp-popover"
+          className="dropdown"
+          style={{ positionAnchor: '--rdp' } as React.CSSProperties}
+        >
+          <DayPicker
+            className="react-day-picker"
+            mode="single"
+            selected={date}
+            hidden={{ after: today }}
+            onSelect={(newDate) => {
+              setIsOpen(false);
+              onDateChange?.(newDate);
+            }}
+            endMonth={today}
+          />
+        </div>
+      )}
     </>
   );
 }
